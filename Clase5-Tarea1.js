5d333dce37edb991aa5aff8bb31cceddbbc4b3a0
JojoUrbina <95663502+JojoUrbina@users.noreply.github.com>
2022-02-14 21:05:53 -0300

    crear const mesesAlAÑo
    puedes crear la variable para que sea mas legible aun.

--- Clase5-Tarea1.js
+++ Clase5-Tarea1.js
@@ -2,12 +2,12 @@
 //TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
 //cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
 // en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
-
+//yo crearia un const mesesEnElAño=12 para que sea mucho mas legible
 const botonCalcular = document.querySelector('#calcular-salario-mensual')
 
 botonCalcular.onclick = function(){
     const salarioAnualUsuario = Number(document.querySelector('#salario-anual-usuario').value)
-    document.querySelector('#salario-mensual').value = salarioAnualUsuario / 12
+    document.querySelector('#salario-mensual').value = salarioAnualUsuario / 12 
     return false
 }
 
